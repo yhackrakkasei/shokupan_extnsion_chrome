cd1f63526ef1512ad666eadccbf61cef8b02bd96
putan <hiranono@gmail.com>
2013-02-16 22:47:13 +0900

    update source

---
+++ script.js
@@ -0,0 +1,13 @@
+//document.body.style.backgroundColor='red';
+//$(function(){
+//    chrome.tabs.executeScript(null, {
+//        "code": "document.body.style.backgroundColor='red'"
+//    });
+//});
+
+//chrome.tabs.getSelected(null, function(tab) {
+//   alert(tab.url);
+//});
+
+console.log(document.URL);
+console.log(CybozuLabs.MD5.calc(document.URL));
